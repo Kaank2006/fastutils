38a86af01f1f722c4f0a06233cec74c034a6d47a
Kaan <kaankucuk2006@gmail.com>
2020-10-22 10:43:01 +0300

    Added fls

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A joke project to show at people who call everything bloat.
 Ironically, many of these are actually slower than normal coreutils.
 
 The set isn't full and i don't plan it to be.
+Compile them with c++17, or you will face errors with fls
---
+++ src/fls.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <filesystem>
+
+int main(const int argc, const char** argv){
+    const std::string path = argv[1];
+    for (const auto &entry : std::filesystem::directory_iterator(path)){
+        std::cout << entry.path() << "/n";
+    }
+}
