c321b5fd2b381c76b59df64b58fa76b7971e4358
Kaan <kaankucuk2006@gmail.com>
2020-10-22 17:48:17 +0300

    Fixed typo: its \n, not /n.

--- src/fls.cpp
+++ src/fls.cpp
@@ -4,6 +4,6 @@
 int main(const int argc, const char** argv){
     const std::string path = argv[1];
     for (const auto &entry : std::filesystem::directory_iterator(path)){
-        std::cout << entry.path() << "/n";
+        std::cout << entry.path() << "\n";
     }
 }
