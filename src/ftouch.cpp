14bbd8c3c969bc02236b50c6c60a12ae00191aec
Kaan <kaankucuk2006@gmail.com>
2020-10-23 10:48:06 +0300

    Made ftouch.cpp more reasonable.

--- src/ftouch.cpp
+++ src/ftouch.cpp
@@ -2,6 +2,10 @@
 
 int main(const int argc, const char** argv)
 {
+    if (argc > 1) {
     std::ofstream file (argv[1]);
+    }
+    else {
+      std::cout << "Please enter a valid filename." << '\n';
+    }
 }
-
