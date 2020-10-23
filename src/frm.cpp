b9ba4e4cf40313641d2a730d6b76d21ab0a10bd7
Kaan <kaankucuk2006@gmail.com>
2020-10-23 11:20:09 +0300

    Made frm.cpp more reasonable.

--- src/frm.cpp
+++ src/frm.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <unistd.h>
+
 int main(const int argc, const char** argv){
+  if (argc <= 1 ) {
+    std::cout << "Please enter a valid name." << '\n';
+    return 1;
+  }
+  else if ( access( argv[1], F_OK ) != -1 ) {
     remove(argv[1]);
+    return 0;
+  }
+  else {
+    std::cout << "No such file." << '\n';
+  }
 }
