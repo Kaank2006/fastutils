dfb8859599526ca3413fe3a23745458b68948c3f
Kaan <kaankucuk2006@gmail.com>
2020-10-23 11:01:28 +0300

    Made fls.cpp reasonable.

--- src/fls.cpp
+++ src/fls.cpp
@@ -2,8 +2,23 @@
 #include <filesystem>
 
 int main(const int argc, const char** argv){
-    const std::string path = argv[1];
-    for (const auto &entry : std::filesystem::directory_iterator(argv[1])){
-        std::cout << entry.path() << "\n";
+  try{
+    if (argc > 1) {
+      const std::string path = argv[1];
+      for (const auto &entry : std::filesystem::directory_iterator(path)){
+      std::cout << entry.path() << "\n";
+      }
     }
+    else {
+      const std::string path = ".";
+      for (const auto &entry : std::filesystem::directory_iterator(path)){
+      std::cout << entry.path() << "\n";
+      }
+    }
+  }
+  catch (...) {
+    std::cout << "Are you sure that such directory exists and you have the permission to read it?" << '\n';
+  }
+
+
 }
