0dcb8966b91b6b01312e4941c13541137abd308a
Kaan <kaankucuk2006@gmail.com>
2020-10-23 10:38:50 +0300

    Made fcat.cpp more reasonable.

--- src/fcat.cpp
+++ src/fcat.cpp
@@ -2,6 +2,13 @@
 #include <fstream>
 
 int main(const int argc, const char** argv){
+
 	const std::ifstream f(argv[1]);
+	if(f.is_open()){
 	std::cout << f.rdbuf();
+	}
+	else {
+		std::cout << "Are you sure there is such file and that you have access to it?" << '\n';
+		return 1;
+	}
 }
